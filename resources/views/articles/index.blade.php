256d6960a108018a8c82d642397c9fcaed0a357e
yunan <yunanchang@yahoo.com.tw>
2024-04-14 22:28:34 +0800

    文章刪除

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,19 +3,26 @@
 @section('main')
     <h1 class="font-thin text-4xl bg-dark">文章列表</h1>
     <p>文章內容</p>
-    <a href="{{route('articles.create')}}">新增文章</a>
-    
-    @foreach($articles as $article)
+    <a href="{{ route('articles.create') }}">新增文章</a>
+
+    @foreach ($articles as $article)
         <div>
             <h2>
-                <a href="{{route('articles.show',$article)}}">
-                {{$article->title}}</a></h2>
-            <p>{{$article->created_at}}由{{$article->user->name}}分享</p>
-       {{-- <a href="{{route('articles.edit',['article'=>$article->id])}}">編輯</a> --}}
-       <a href="{{route('articles.edit',$article)}}">編輯</a>
-    
-    </div>
+                <a href="{{ route('articles.show', $article) }}">
+                    {{ $article->title }}</a>
+            </h2>
+            <p>{{ $article->created_at }}由{{ $article->user->name }}分享</p>
+            {{-- <a href="{{route('articles.edit',['article'=>$article->id])}}">編輯</a> --}}
+            <div class="flex">
+                <a href="{{ route('articles.edit', $article) }}">編輯</a>
+                <form action="{{ route('articles.destroy', $article) }}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button type="submit">刪除</button>
+                </form>
+            </div>
 
+        </div>
     @endforeach
-    {{$articles->links()}}
-    @endsection
+    {{ $articles->links() }}
+@endsection
